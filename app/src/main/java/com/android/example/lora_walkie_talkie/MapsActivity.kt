d7f600a5256657b2ee869e1020f04ff026dd8c75
Chiguez <rodriguezpham@gmail.com>
2022-05-31 20:06:56 -0700

    Updated GPS button to update the message log with longitude and latitude coordinates. It also updates the google maps to drop a pin to the correct location.

--- app/src/main/java/com/android/example/lora_walkie_talkie/MapsActivity.kt
+++ app/src/main/java/com/android/example/lora_walkie_talkie/MapsActivity.kt
@@ -1,7 +1,18 @@
 package com.android.example.lora_walkie_talkie
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.Debug.getLocation
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,10 +22,19 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.android.example.lora_walkie_talkie.databinding.ActivityMapsBinding
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+import android.widget.TextView
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener{
+    private var lat: Double= 0.0
+    private var long: Double= 0.0
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var tvGpsLocation: TextView
+    private val locationPermissionCode = 2
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +42,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        title = "KotlinApp"
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            getLocation()
+        }
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -40,10 +66,45 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
+        var sydney = LatLng(31.0, 151.0)
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        if (lat != null) {
+             sydney = LatLng(lat, long)
+        }
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+    }
+    override fun onLocationChanged(location: Location) {
+        lat = location.latitude
+        long = location.longitude
+        tvGpsLocation = findViewById(R.id.textView)
+        tvGpsLocation.text = "Latitude: " + location.latitude + " , Longitude: " + location.longitude
+
+        addLocMap(lat,long,"Here")
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun addLocMap(lat:Double,long:Double,pin_title:String) {
+        var current_location = LatLng(lat, long)
+        mMap.addMarker(MarkerOptions().position(current_location ).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(current_location ))
     }
 }
